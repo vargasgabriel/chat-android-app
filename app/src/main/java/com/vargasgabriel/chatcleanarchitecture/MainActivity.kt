b4a088cfbdc80a580e77e0a9746b7b9b98ffe908
Gabriel Vargas <assuncao.gabriel12@gmail.com>
2022-06-07 23:08:09 -0300

    Initial commit

---
+++ app/src/main/java/com/vargasgabriel/chatcleanarchitecture/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vargasgabriel.chatcleanarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.time.OffsetDateTime
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
