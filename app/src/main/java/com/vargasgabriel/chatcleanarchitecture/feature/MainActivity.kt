bf3bc4c809120acd0839e5946fbc042a76586d57
Gabriel Vargas <assuncao.gabriel12@gmail.com>
2022-06-08 00:03:05 -0300

    Cria e usa navigation xml no MainActivity

---
+++ app/src/main/java/com/vargasgabriel/chatcleanarchitecture/feature/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.vargasgabriel.chatcleanarchitecture.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.vargasgabriel.chatcleanarchitecture.R
+import com.vargasgabriel.chatcleanarchitecture.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navHostFragment.navController.setGraph(R.navigation.nav_graph)
+    }
+}
